ace16f3999aa1f9aebe790b7991b6243ff2a7662
jero98772 <jero98772@protonmail.com>
2023-02-10 22:24:18 -0500

    frist commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # integral-looks-like-pipe
+![logo](https://github.com/jero98772/integral-looks-like-pipe/blob/main/misc/logo.jpeg)
+
 integral calculator in python with metaprograming technics
+
+	func=lambda x:eval("x**2+sqrt(x)")
+
+this line convert string to python funcion
+
+**install dependencies**
+
+		pip install numpy
+
+**Run**
+
+	python main.py
+
+
+#Screenshots
+![1](https://github.com/jero98772/integral-looks-like-pipe/blob/main/misc/1.png)
+
---
+++ calcular_integrales.py
@@ -0,0 +1,31 @@
+from math import *
+import numpy as np
+def integral(xi,xf,n,func):
+	times=1/n#(xf-xi)
+	nums=np.arange(xi,xf,times)
+	applay=np.vectorize(func)
+	dots=applay(nums)
+	return dots
+def main():
+	print("integral from")
+	try:
+		xi=int(input("from ...[default: 0]\n"))
+	except:
+		xi=0
+	try:
+		xf=int(input("end ... [default: 10]\n"))
+	except:
+		xf=10
+	try:
+		n=int(input("how many steps?...[default: 10]"))
+	except:
+		n=10
+	try:
+		funcs=input("please input your function on x:[default: x**2+sqrt(x)]\n")
+		func=lambda x:eval(funcs)	
+	except:
+		func=lambda x:eval("x**2+sqrt(x)")
+	#interpretate functios string to code  
+	dots=integral(xi,xf,n,func)
+	print(np.sum(dots)/n)
+main()
